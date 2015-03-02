625a24ba2a77fff90f8c08089440113c8d04aaf2
olehalvor <olli408@hotmail.com>
2015-03-02 11:40:49 +0100

    Working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,35 +2,53 @@ package com.company;
 
 import java.io.*;
 import java.net.*;
+
 import org.json.*;
 
 public class Main {
+    public static int i;
 
     public static void main(String args[]) throws Exception
     {
         String clientSentence;
-        String capitalizedSentence;
         ServerSocket welcomeSocket = new ServerSocket(6789);
-        JSONObject obj = new JSONObject();
+        System.out.println("Server started");
+        i = 0;
 
         while(true)
         {
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             clientSentence = inFromClient.readLine();
+            System.out.println("Meld mottat");
             decodeJson(clientSentence);
+            writeJsonToFile(clientSentence);
+            i++;
         }
-
-
     }
 
     private static void decodeJson(String jsonString){
         try {
             JSONObject obj = new JSONObject(jsonString);
-        } catch (JSONException e) {
+            System.out.println(obj.toString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    private static void writeJsonToFile(String jsonString) throws Exception{
+        String filePath;
+        boolean done = false;
+        while (!done){
+            filePath = "fall"+i+".json";
+            File f = new File(filePath);
+            if (!(f.exists())){
+                byte dataToWrite[] = jsonString.getBytes();
+                FileOutputStream out = new FileOutputStream(filePath);
+                out.write(dataToWrite);
+                out.close();
+                done = true;
+            }else i++;
+        }
     }
 }
