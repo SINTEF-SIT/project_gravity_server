e316b3e4b88109a448b996d5faee7fb32281d498
OleHalvor <olli408@hotmail.com>
2015-03-24 14:49:11 +0100

    Supports linear acceleration - better error messages

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,58 +9,65 @@ public class Main {
     static String fallID = "1";
     static String fallArr;
     static int fallNR = 1;
+    static int magLen = 0;
+    static int linAccLen =0;
 
     public static void main(String args[]) throws Exception
     {
         String clientSentence;
         ServerSocket welcomeSocket = new ServerSocket(8765);
         System.out.println("Server started");
-
         while(true)
         {
+            try{
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             clientSentence = inFromClient.readLine();
             decodeJson(clientSentence);
-            writeJsonToFile(clientSentence);
+            writeJsonToFile(clientSentence);}
+            catch (Exception e){
+                System.out.println("ERROR: failed to connect to sender");}
         }
     }
 
-    //add check to see if file is too small
-
-    private static void decodeJson(String jsonString) throws Exception{
-        JSONObject obj = new JSONObject(jsonString);
-        String temp ="";
+    private static void decodeJson(String jsonString){
         try {
-            temp = obj.get("test_id").toString().replaceAll("\\s","");
-            fallID = temp;
+            JSONObject obj = new JSONObject(jsonString);
+            fallID = obj.get("test_id").toString().replaceAll("\\s","");
             fallArr = obj.get("fall_detected_at_times").toString();
-            //fallNR = Integer.parseInt(obj.get("fall_nr").toString());
-            //System.out.println(fallID + ", " + obj.get("fall_detected_at_times"));
+            JSONObject sensorData = obj.getJSONObject("sensor_data");
+            JSONArray linAcc = sensorData.getJSONArray("linear_acceleration");
+            JSONArray magField = sensorData.getJSONArray("magnetic_field_data");
+            linAccLen = linAcc.length();
+            magLen = magField.length();
         }catch (Exception e){
-            e.printStackTrace();
+            System.out.println("ERROR: failed to decode file");
         }
     }
 
-    private static void writeJsonToFile(String jsonString) throws Exception{
-        String filePath;
-        boolean done = false;
-        fallNR=0;
-        int count = 0;
-        while (!done){
-            filePath = "ID"+fallID+"NR"+count+".json";
-            File f = new File(filePath);
-            if (!(f.exists())){
-                byte dataToWrite[] = jsonString.getBytes();
-                FileOutputStream out = new FileOutputStream(filePath);
-                out.write(dataToWrite);
-                out.close();
-                done = true;
-
-                if (fallArr.length()>=5){
-                    System.out.println(fallID + ", " +count+": Fall Detected!");
-                }else System.out.println(fallID + ", " +count+": No fall");
-            }else count++;
-        }
+    private static void writeJsonToFile(String jsonString){
+        try {
+            String filePath;
+            boolean done = false;
+            fallNR = 0;
+            int count = 1;
+            while (!done) {
+                filePath = "ID" + fallID + "NR" + count + ".json";
+                File f = new File(filePath);
+                if (!(f.exists())) {
+                    byte dataToWrite[] = jsonString.getBytes();
+                    FileOutputStream out = new FileOutputStream(filePath);
+                    out.write(dataToWrite);
+                    out.close();
+                    done = true;
+                    if (fallArr.length() >= 5) {
+                        System.out.println(fallID + ", " + count + ": Fall Detected!    linAccLen: " + linAccLen + ", magLen: " + magLen);
+                    } else
+                        System.out.println(fallID + ", " + count + ": No fall          linAccLen: " + linAccLen + ", magLen: " + magLen);
+                    //if (accLen<100) System.out.println("WARNING: too few acceleration data");
+                } else count++;
+            }
+        }catch (Exception e){
+            System.out.println("ERROR: failed to write file to disk");}
     }
 }
